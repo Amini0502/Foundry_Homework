f06d001f91399ed16990fc3d6329776686d1d8d0
Amini0502 <amasth@my.bridgeport.edu>
2025-04-19 01:17:50 -0400

    Fix: Rename test_updatePages to test_update_pages

--- test/Books.t.sol
+++ test/Books.t.sol
@@ -18,7 +18,7 @@ contract TestBooks is Test {
         assertEq(book.pages, 100);
     }
 
-    function test_updatePages() public {
+    function test_update_Pages() public {
         books.update_pages(250);
         Books.Book memory updatedBook = books.get_book();
         assertEq(updatedBook.pages, 250);
